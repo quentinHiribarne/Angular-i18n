7472ceee7b480e13e01f1820c8d185d7f6d7b9dd
Quentin Hiribarne Pionnier <quentin.pionnier@gmail.com>
2023-05-10 16:39:20 +0200

    mirgate from npm to pnpm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormComponent } from './form/form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatCardModule } from '@angular/material/card';
-import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
 import { LangSelectComponent } from './lang-select/lang-select.component';
+import { FormComponent } from './form/form.component';
 import { I18nModule } from './core/i18n/i18n.module';
 
 @NgModule({
