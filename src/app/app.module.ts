2248810e57ce002015f3df133091c09c7b98788c
Quentin Hiribarne Pionnier <quentin.pionnier@gmail.com>
2023-05-10 14:34:46 +0200

    Add language change at runtime

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,28 +7,27 @@ import { FormComponent } from './form/form.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatCardModule } from '@angular/material/card';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LangSelectComponent } from './lang-select/lang-select.component';
+import { I18nModule } from './core/i18n/i18n.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FormComponent,
-    LangSelectComponent
-  ],
+  declarations: [AppComponent, FormComponent, LangSelectComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatButtonModule,
     MatSelectModule,
+    MatButtonToggleModule,
     MatRadioModule,
     MatCardModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [I18nModule.setLocale(), I18nModule.setLocaleId()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/lang-select/lang-select.component.ts
+++ src/app/lang-select/lang-select.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-lang-select',
   templateUrl: './lang-select.component.html',
-  styleUrls: ['./lang-select.component.css']
+  styleUrls: ['./lang-select.component.css'],
 })
 export class LangSelectComponent {
-  selected = 'option2';
+  // Get the navigator language
+  navigatorLang = window.navigator.language;
+
+  // If local set in localStorage set default language to it else set it to navigator language or 'en' if not available
+  defaultLang = localStorage.getItem('locale') || this.navigatorLang || 'en';
+
+  changeLocale(locale: string) {
+    // If no locale is provided, default to 'en'
+    if (!locale) {
+      locale = this.defaultLang;
+    }
+
+    // Store the user's preferred locale in localStorage
+    localStorage.setItem('locale', locale);
+
+    // Reload the page to apply the new locale
+    location.reload();
+  }
 }
